237c8f6ebeb0fc237ff524d6af09fb93f2cdff60
memo figueredo <memof80@gmail.com>
2020-09-21 16:10:14 -0500

    task 2 ready

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject  } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Leader } from '../../shared/leader';
+import { LeaderService } from '../providers/leader.service';
+
 
 @Component({
   selector: 'app-about',
@@ -8,10 +11,15 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class AboutPage implements OnInit {
   public about: string;
+  leaders: Leader[];
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute,
+              private leaderService: LeaderService,
+              @Inject('BaseURL') public BaseURL: any ) { }
 
   ngOnInit() {
+    this.leaderService.getLeaders()
+    .subscribe((leaders) =>  this.leaders  = leaders);
     this.about = this.activatedRoute.snapshot.paramMap.get('id');
   }
 
