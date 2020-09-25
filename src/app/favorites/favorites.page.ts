65862432cf0b73a86bdde688cbc02d75f54db857
memo figueredo <memof80@gmail.com>
2020-09-25 18:31:44 -0500

    Alerts, Toasts and Loading

--- src/app/favorites/favorites.page.ts
+++ src/app/favorites/favorites.page.ts
@@ -3,7 +3,8 @@ import { Dish } from '../../shared/dish';
 import { FavoriteService } from '../providers/favorite.service';
 import { baseURL } from '../../shared/baseurl';
 import {  NavController, NavParams} from '@ionic/angular';
-import { IonItemSliding , LoadingController, ToastController } from '@ionic/angular';
+import { IonItemSliding , LoadingController, ToastController, AlertController } from '@ionic/angular';
+import { async } from '@angular/core/testing';
 
 
 
@@ -18,7 +19,8 @@ export class FavoritesPage implements OnInit {
   errMess: string;
 
   constructor(private favoriteservice: FavoriteService,
-              public loadingController: LoadingController,
+              private loadingCtrl: LoadingController,
+              private alertCtrl: AlertController,
               public toastCtrl: ToastController,
               @Inject('BaseURL') public BaseURL: any) { }
 
@@ -29,16 +31,40 @@ export class FavoritesPage implements OnInit {
   }
 
    async deleteFavorite(item: IonItemSliding, id: string) {
-    this.favoriteservice.deleteFavorite(id)
-      .subscribe(favorites => this.favorites = favorites,
-        errmess => this.errMess = errmess);
-    const toast = this.toastCtrl.create({
-      message: 'Dish' + ' ' + id + ' ' + 'deleted suceessfully',
-      duration: 3000,
-      color: 'danger'
-    });
-    (await toast).present();
-    item.close();
+     console.log('delete', id);
+     const alert = this.alertCtrl.create({
+      header: 'Confirm Delete',
+      message: 'Do you want to delete Dish' + ' ' + id,
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Delete cancelled');
+          }
+        },
+        {
+          text: 'Delete',
+           handler: async () => {
+           const loading = this.loadingCtrl.create({
+              message: 'Deleting...'
+            });
+           const toast = this.toastCtrl.create({
+              message: 'Dish' + ' ' + id + ' ' + 'deleted suceessfully',
+              duration: 3000,
+              color: 'danger'
+            });
+           (await loading).present();
+           this.favoriteservice.deleteFavorite(id)
+          .subscribe( async favorites => {this.favorites = favorites; (await loading).dismiss();
+                                          (await (toast)).present(); },
+           async errmess => {this.errMess = errmess; (await loading).dismiss(); });
+          }
+        }
+      ]
+     });
+     (await (alert)).present();
+     item.close();
   }
 
 
